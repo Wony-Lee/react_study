2680d0b7e85aefeab4a42de4abb4e3964059f5ac
Wony-Lee <a1_11_11@naver.com>
2021-11-10 15:54:51 +0900

    useState multiple

--- src/components/useComp/Say.js
+++ src/components/useComp/Say.js
@@ -4,12 +4,34 @@ const Say = () => {
     const [message, setMessage] = useState("1");
     const onClickEnter = () => setMessage("Hi");
     const onClickLeave = () => setMessage("Bye");
+
+    const [color, setColor] = useState("black");
     return (
         <>
             <div>
                 <button onClick={onClickEnter}>입장</button>
                 <button onClick={onClickLeave}>퇴장</button>
-                <p>{message}</p>
+                <p style={{ color }}>{message}</p>
+                <button
+                    style={{ color: "red" }}
+                    onClick={() => setColor("red")}
+                >
+                    red
+                </button>
+
+                <button
+                    style={{ color: "green" }}
+                    onClick={() => setColor("green")}
+                >
+                    green
+                </button>
+
+                <button
+                    style={{ color: "blue" }}
+                    onClick={() => setColor("blue")}
+                >
+                    blue
+                </button>
             </div>
         </>
     );
