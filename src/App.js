c26e6cc66766b7aaa68398ffec3d3b43ff75d223
Wony <a1_11_11@naver.com>
2021-11-11 17:22:53 +0900

    컴포넌트에 ref 달기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import "./App.css";
-import ValidationSample from "./components/ValidationSample";
+import ScrollBoxClass from "./components/ScrollBoxClass";
 
-function App() {
-  return (
-    <>
-      <ValidationSample />
-    </>
-  );
+import React, { Component } from "react";
+
+// function App() {
+//   return (
+//     <>
+//       <ScrollBoxClass />
+//       <button onClick={() => this.scrollBox.scrollToButton()}>맨 밑으로</button>
+//     </>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBoxClass ref={ref => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToButton()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
