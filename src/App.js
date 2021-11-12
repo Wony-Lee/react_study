21f4d1780bdcaf5549df667cd04c9dedb90a8462
Wony <a1_11_11@naver.com>
2021-11-12 17:48:01 +0900

    LifeCycleComponent

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import "./App.css";
-import IterationSample from "./components/IterationSample";
-//import ScrollBoxClass from "./components/ScrollBoxClass";
+import LifeCycleSample from "./components/LifeCycleSample";
 
-//import React, { Component } from "react";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <>
-      <IterationSample />
-    </>
-  );
+// function App() {
+//   return (
+//     <>
+//       <LifeCycleSample />
+//     </>
+//   );
+// }
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <ScrollBoxClass ref={ref => (this.scrollBox = ref)} />
-//         <button onClick={() => this.scrollBox.scrollToButton()}>
-//           맨 밑으로
-//         </button>
-//       </div>
-//     );
-//   }
-// }
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+  render() {
+    return (
+      <>
+        <div>
+          <button onClick={this.handleClick}>랜덤 생삭</button>
+          <LifeCycleSample color={this.state.color} />
+        </div>
+      </>
+    );
+  }
+}
 
 export default App;
