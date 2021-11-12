f7a4dcc451e03b653d3b15ab154689b69251737b
Wony <68363082+Wony-Lee@users.noreply.github.com>
2021-11-13 01:19:17 +0900

    step2

--- src/App.js
+++ src/App.js
@@ -1,43 +1,7 @@
 import "./App.css";
-import LifeCycleSample from "./components/LifeCycleSample";
 
-import React, { Component } from "react";
-import ErrorBoundary from "./components/ErrorBoundary";
-
-// function App() {
-//   return (
-//     <>
-//       <LifeCycleSample />
-//     </>
-//   );
-// }
-
-function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
-
-class App extends Component {
-  state = {
-    color: "#000000",
-  };
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
-  render() {
-    return (
-      <>
-        <div>
-          <button onClick={this.handleClick}>랜덤 생삭</button>
-          <ErrorBoundary>
-            <LifeCycleSample color={this.state.color} />
-          </ErrorBoundary>
-        </div>
-      </>
-    );
-  }
+function App() {
+  return <></>;
 }
 
 export default App;
