59cf4a4b2064eed1e2115ec0205a39240d62e20b
Wony <68363082+Wony-Lee@users.noreply.github.com>
2021-11-14 19:26:47 +0900

    checked and delete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useRef } from "react";
 import "./App.css";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 import TodoTemplate from "./components/TodoTemplate";
+
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일${i}`,
+      checked: false,
+    });
+  }
+}
 
 const App = () => {
   const [todos, setTodos] = useState([
@@ -22,11 +33,42 @@ const App = () => {
       checked: false,
     },
   ]);
+  const nextId = useRef(4);
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; // nextId 1씩 증가
+    },
+    [todos]
+  );
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((item) => item.id !== id));
+    },
+    [todos]
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((item) =>
+          item.id === id ? { ...item, checked: !item.checked } : item
+        )
+      );
+    },
+    [todos]
+  );
+
   return (
     <>
       <TodoTemplate>
-        <TodoInsert />
-        <TodoList todos={todos} />
+        <TodoInsert onInsert={onInsert} />
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
       </TodoTemplate>
     </>
   );
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,17 +7,20 @@ import {
 import cn from "classnames";
 import "./style/TodoListItem.scss";
 
-const TodoListItem = ({ todo }) => {
-  const { text, checked } = todo;
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+  const { id, text, checked } = todo;
   return (
     <>
       <div className="TodoListItem">
-        <div className={cn("checkbox", { checked })}>
+        <div
+          className={cn("checkbox", { checked })}
+          onClick={() => onToggle(id)}
+        >
           {checked ? <IoCheckbox /> : <IoCheckboxOutline />}
 
           <div className="text">{text}</div>
         </div>
-        <div className="remove">
+        <div className="remove" onClick={() => onRemove(id)}>
           <IoRemoveCircleOutline />
         </div>
       </div>
