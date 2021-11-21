96ab362d11c76850312e0e741d54540f402df2b0
Wony-Lee <a1_11_11@naver.com>
2021-11-21 16:02:59 +0900

    react-router v6

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import AppLayout from "./components/AppLayout";
-import Test from "./components/Test";
+import Profile from "./components/Profile";
 
 const App = () => {
-  return (
-    <>
-      <AppLayout></AppLayout>
+    return (
+        <>
+            <AppLayout></AppLayout>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route index path="about" element={<About />} />
-      </Routes>
-    </>
-  );
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route index path="about" element={<About />} />
+                <Route path="profiles/:username" element={<Profile />}></Route>
+            </Routes>
+        </>
+    );
 };
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return (
-    <>
-      <Link to="/">Home</Link>
-      <Link to="test">Test</Link>
-      <Link to="about">About</Link>
-    </>
-  );
+    return (
+        <>
+            <Link to="/">Home</Link>
+            <Link to="test">Test</Link>
+            <Link to="about">About</Link>
+            <Link to="profiles/wony">Wony프로필</Link>
+            <Link to="profiles/gildong">gildong 프로필</Link>
+        </>
+    );
 };
 
 export default NavBar;
